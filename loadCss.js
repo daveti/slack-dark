1ea26d1df04af0bb9f2d35fdf6faafcbfa00a12f
Dave Tian <root@davejingtian.org>
2018-10-25 12:16:29 -0400

    daveti is in my tcb

--- loadCss.js
+++ loadCss.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
  $.ajax({
-   url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+   //url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+   url: 'https://raw.githubusercontent.com/daveti/slack-dark/master/black.css',
    success: function(css) {
      $("<style></style>").appendTo('head').html(css);
      $("<style></style>").appendTo('head').html('#reply_container.upload_in_threads .inline_message_input_container {background: padding-box #545454}');
